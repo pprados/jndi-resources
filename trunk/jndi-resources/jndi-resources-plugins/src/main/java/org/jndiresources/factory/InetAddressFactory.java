a5a947ce6435adee820038d354b9788aff1a1bc3
philippe.prados <philippe.prados@9a2aa842-8647-0410-b233-5d07bf6e8563>
2008-03-04 11:39:50 +0000

    Initial import.

---
+++ trunk/jndi-resources/jndi-resources-plugins/src/main/java/org/jndiresources/factory/InetAddressFactory.java
@@ -0,0 +1,62 @@
+package org.jndiresources.factory;
+
+/*
+ * Copyright 2008 Philippe Prados.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Hashtable;
+
+import javax.naming.Context;
+import javax.naming.Name;
+import javax.naming.NamingException;
+import javax.naming.Reference;
+import javax.naming.spi.ObjectFactory;
+
+/**
+ * Inet adresse object factory.
+ * @version 1.0
+ * @since 1.0
+ */
+public class InetAddressFactory implements ObjectFactory
+{
+	/**
+	 * {@inheritDoc}
+	 */
+	public Object getObjectInstance(Object obj, Name name,
+			Context nameCtx, Hashtable environment)
+			throws NamingException
+	{
+		try
+		{
+			if (obj instanceof Reference)
+			{
+				Reference ref = (Reference) obj;
+				if (!"java.net.InetAddress".equals(ref.getClassName()))
+						throw new NamingException("Invalide type "+ref.getClassName());
+				return InetAddress.getByName((String)ref.get("value").getContent());
+			}
+			return null;
+		}
+		catch (UnknownHostException e)
+		{
+            NamingException ne = new NamingException(e.getMessage());
+            ne.setRootCause(e);
+            throw ne;
+		}
+	}
+
+}
---
+++ trunk/jndi-resources/jndi-resources-plugins/src/main/java/org/jndiresources/mbean/ReferenceServiceMBean.java
@@ -0,0 +1,51 @@
+package org.jndiresources.mbean;
+
+/*
+ * Copyright 2008 Philippe Prados.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import javax.naming.NamingException;
+
+import org.w3c.dom.Element;
+
+/**
+ * Interface for Reference service.
+ * 
+ * @version 1.0
+ * @since 1.0
+ */
+public interface ReferenceServiceMBean
+{
+	String getJNDIName();
+
+	void setJNDIName(String jndiName) throws NamingException;
+
+	String getType();
+
+	void setType(String type) throws NamingException;
+
+	String getFactory();
+
+	void setFactory(String factory) throws NamingException;
+
+	Element getConfiguration();
+
+	void setConfiguration(final Element element) throws NamingException;
+
+	void start() throws NamingException;
+
+	void stop();
+
+}
